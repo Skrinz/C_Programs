8abb17935eadfcd8ea7a1fe7da36262637a68671
Skrinz <ranzcabas066@gmail.com>
2024-12-01 19:29:15 +0800

    c program setup

--- Tester.c
+++ Tester.c
@@ -1,27 +1,4 @@
 #include <stdio.h>
-
-void main (){
-	//declare variables
-	int num1;
-	int num2;
-	int hcf;
-	int max;
-	
-	//ask for user input
-	printf("First number: ");
-	scanf("%d", &num1);
-	printf("Second number: ");
-	scanf("%d", &num2);
-	
-	//check what is the minimum number
-	max = (num1 > num2)? num2: num1;
-	
-	//process the input
-	for(int i = 1; i <= ma ; ++i){
-		//check if i is a factor of both integers
-		if (num1 % i == 0 && num2 % i == 0)
-			hcf = i;	
-	}
-	//print the HCF of both numbers
-	printf("HCF of %d and %d is %d", num1, num2, hcf);
+int main(){
+	printf("helloworld\n\n");
 }
